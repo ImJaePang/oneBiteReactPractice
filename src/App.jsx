47a62cadf70f2f6f4e5def953194fee764fbaf18
ImJaePang <narushel@naver.com>
2025-12-28 20:03:25 +0900

    useReducer 연습

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import Exam from "./components/Exam";
 
 const mockData = [
     {
@@ -65,9 +66,16 @@ function App() {
 
     return (
         <div className="App">
-            <Header />
+            <Exam/>
+
+
+
+
+            
+            {/* <Header />
             <Editor onCreate={onCreate} />
-            <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+            <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+
         </div>
     );
 }
