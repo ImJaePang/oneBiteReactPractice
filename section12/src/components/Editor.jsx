26d637723bf05d519fc769d0d3ed38585b7d721f
Jaeyoung Im <narushel09@gmail.com>
2026-03-24 12:30:34 +0900

    NEW 페이지 구현하기 UI부분

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,31 +1,40 @@
 import './Editor.css';
 import EmotionItem from './EmotionItem';
+import Button from './Button';
 
-const arrEmotionObject = [
+const emotionList = [
     {emotionId : 1, emotionName:"완전좋음"},
     {emotionId : 2, emotionName:"그냥좋음"},
     {emotionId : 3, emotionName:"적당함"},
+    {emotionId : 4, emotionName:"나쁨"},
+    {emotionId : 5, emotionName:"끔찍함"},
 ]
 
 const Editor = () => {
+
+    const emotionid = 1;
+
     return <div className='Editor'>
         <section className='date_section'>
             <h4>오늘의 날짜</h4>
             <input type='date' />
         </section>
-        <section className='emoton_section'>
+        <section className='emotion_section'>
             <h4>오늘의 감정</h4>
-            <div>
-                {arrEmotionObject.map((emotion)=>{
-                    return <EmotionItem emotionId={emotion.emotionId} emotionName={emotion.emotionName} />
+            <div className='emotion_list_warapper'>
+                {emotionList.map((item)=>{
+                    return <EmotionItem key={item.emotionId} {...item} isSelected={item.emotionId === emotionid} />
                 })}
-                <EmotionItem />
-                <EmotionItem />
-                <EmotionItem />
             </div>
         </section>
-        <section className='content_section'></section>
-        <section className='button_section'></section>
+        <section className='content_section'>
+            <h4>오늘의 일기</h4>
+            <textarea placeholder='오늘은 어땠나요?'/>
+        </section>
+        <section className='button_section'>
+                <Button text={"취소하기"} />
+                <Button text={"직상 어니려"} type={"POSITIVE"} />
+        </section>
     </div>
 }
 
