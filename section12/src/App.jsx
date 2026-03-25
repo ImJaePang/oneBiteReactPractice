aa2582896a1d9525d2ee96c65b5bcec52fee4c36
Jaeyoung Im <narushel09@gmail.com>
2026-03-25 12:29:59 +0900

    New 페이지 구현하기 기능2번째

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -7,102 +7,99 @@ import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 
-
 const mockData = [
-    {
-        id: 1,
-        createdDate: new Date("2026-03-21").getTime(),
-        emotionId: 1,
-        content: "1번 일기 내용",
-    },
-    {
-        id: 2,
-        createdDate: new Date("2026-03-20").getTime(),
-        emotionId: 2,
-        content: "2번 일기 내용",
-    },
-    {
-        id: 3,
-        createdDate: new Date("2026-01-17").getTime(),
-        emotionId: 3,
-        content: "3번 일기 내용",
-    },
+  {
+    id: 1,
+    createdDate: new Date("2026-03-21").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date("2026-03-20").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2026-01-17").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
 ];
 
 function reducer(state, action) {
-    switch (action.type) {
-        case "CREATE":
-            return [action.data, ...state];
-        case "UPDATE":
-            return state.map((item) =>
-                String(item.id) === String(action.data.id) ? action.data : item
-            );
-        case "DELETE":
-            return state.filter(
-                (item) => String(item.id) !== String(action.id)
-            );
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item,
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-    const idRef = useRef(3);
+  const idRef = useRef(4);
 
-    const [data, dispatch] = useReducer(reducer, mockData);
+  const [data, dispatch] = useReducer(reducer, mockData);
 
-    const onCreate = (createdDate, emotionId, content) => {
-        // 새로운 일기를 추가하는 기능
-        dispatch({
-            type: "CREATE",
-            data: {
-                id: idRef.current++,
-                createdDate,
-                emotionId,
-                content,
-            },
-        });
-    };
+  const onCreate = (createdDate, emotionId, content) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
 
-    const onUpdate = (id, createdDate, emotionId, content) => {
-        // 새로운 일기를 추가하는 기능
-        dispatch({
-            type: "UPDATE",
-            data: {
-                id,
-                createdDate,
-                emotionId,
-                content,
-            },
-        });
-    };
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
 
-    const onDelete = (id) => {
-        // 새로운 일기를 추가하는 기능
-        dispatch({
-            type: "DELETE",
-            id,
-        });
-    };
+  const onDelete = (id) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
 
-    return (
-        <>
-            <DiaryStateContext.Provider value={data}>
-                <DiaryDispatchContext.Provider value={{onCreate, onDelete, onUpdate,}}>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/new" element={<New />} />
-                        <Route path="/diary/:id" element={<Diary />} />
-                        <Route path="/edit/:id" element={<Edit />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </DiaryDispatchContext.Provider>
-            </DiaryStateContext.Provider>
-        </>
-    );
+  return (
+    <>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onDelete, onUpdate }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    </>
+  );
 }
 
 export default App;
--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,67 +1,116 @@
-import './Editor.css';
-import EmotionItem from './EmotionItem';
-import Button from './Button';
-import { useState } from 'react';
+import "./Editor.css";
+import EmotionItem from "./EmotionItem";
+import Button from "./Button";
+import { useContext, useReducer, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const emotionList = [
-    {emotionId : 1, emotionName:"완전좋음"},
-    {emotionId : 2, emotionName:"그냥좋음"},
-    {emotionId : 3, emotionName:"적당함"},
-    {emotionId : 4, emotionName:"나쁨"},
-    {emotionId : 5, emotionName:"끔찍함"},
-]
+  { emotionId: 1, emotionName: "완전좋음" },
+  { emotionId: 2, emotionName: "그냥좋음" },
+  { emotionId: 3, emotionName: "적당함" },
+  { emotionId: 4, emotionName: "나쁨" },
+  { emotionId: 5, emotionName: "끔찍함" },
+];
 
 const getStringedDate = (targetDate) => {
-    let year = targetDate.getFullYear();
-    let month = targetDate.getMonth() + 1;
-    let date = targetDate.getDate();
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
 
-    if (month < 10) {month = `0${month}`}
-    if (date < 10) {date = `0${date}`}
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
 
-    return `${year}-${month}-${date}`
-}
+  return `${year}-${month}-${date}`;
+};
 
-const Editor = () => {
-    const [input, setInput] = useState({
-        createdDate : new Date(),
-        emotionid : 3,
-        content : "",
-    });
+const Editor = ({ onSubmit }) => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
 
-    const onChangeInput = (e) => {
-        console.log(`${e.target.name} : ${e.target.value}`);
+  const onChangeInput = (e) => {
+    // console.log(`${e.target.name} : ${e.target.value}`);
+    let name = e.target.name;
+    let value = e.target.value;
 
-        // setInput({
-            
-        //     createdDate : e.target.value,
-        // });
+    if (name === "createdDate") {
+      value = new Date(value);
     }
 
-    const emotionid = 1;
+    setInput({
+      ...input,
+      [name]: value,
+    });
 
-    return <div className='Editor'>
-        <section className='date_section'>
-            <h4>오늘의 날짜</h4>
-            <input name='createdDate' type='date' value={getStringedDate(input.createdDate)} onChange={onChangeInput}/>
-        </section>
-        <section className='emotion_section'>
-            <h4>오늘의 감정</h4>
-            <div className='emotion_list_warapper'>
-                {emotionList.map((item)=>{
-                    return <EmotionItem key={item.emotionId} {...item} isSelected={item.emotionId === emotionid} />
-                })}
-            </div>
-        </section>
-        <section className='content_section'>
-            <h4>오늘의 일기</h4>
-            <textarea placeholder='오늘은 어땠나요?'/>
-        </section>
-        <section className='button_section'>
-                <Button text={"취소하기"} />
-                <Button text={"직상 어니려"} type={"POSITIVE"} />
-        </section>
+    // console.log("input : ", input);
+  };
+
+  //   const emotionid = 1;
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
+
+  const nav = useNavigate();
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h4>오늘의 날짜</h4>
+        <input
+          name="createdDate"
+          type="date"
+          value={getStringedDate(input.createdDate)}
+          onChange={onChangeInput}
+        />
+      </section>
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_warapper">
+          {emotionList.map((item) => {
+            return (
+              <EmotionItem
+                onClick={() =>
+                  onChangeInput({
+                    target: {
+                      name: "emotionId",
+                      value: item.emotionId,
+                    },
+                  })
+                }
+                key={item.emotionId}
+                {...item}
+                isSelected={item.emotionId === input.emotionId}
+              />
+            );
+          })}
+        </div>
+      </section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea
+          name="content"
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+          value={input.content}
+        />
+      </section>
+      <section className="button_section">
+        <Button text={"취소하기"} onClick={() => nav(-1)} />
+        <Button
+          text={"작성 완료"}
+          type={"POSITIVE"}
+          onClick={onClickSubmitButton}
+        />
+      </section>
     </div>
-}
+  );
+};
 
 export default Editor;
