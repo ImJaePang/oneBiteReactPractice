b60cc7ca96d1f7e986b02fe941b0c2b8ed74d26d
Jaeyoung Im <narushel09@gmail.com>
2026-03-25 12:54:10 +0900

    강의듣기전 Edit페이지 일부수정

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,11 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import Button from "../components/Button";
 
 const Edit = () => {
 
     const params = useParams();
     console.log(params);
+    const nav = useNavigate();
 
-    return <div>{params.id} 번 일기 수정 입니다.</div>;
+    const {onCreate} = useContext(DiaryDispatchContext);
+    const onSubmit = (input) => {
+        onUpdate(params.id, input.createdDate.getTime(), input.emotionId, input.content);
+        nav("/", {replace : true});
+    };
+
+    const {onDelete} = useContext(DiaryDispatchContext)
+    const onClickDeleteButton = () => {
+        onDelete(params.id);
+    }
+    
+    return <div>
+        <Header title={"수정하기"}
+        leftChild={<Button text={"< 뒤로 가기"} onClick={()=>nav(-1)}/>} 
+        rightChild={<Button text={"삭제하기"} onClick={onClickDeleteButton}/>} 
+        />
+        <Editor onSubmit={onSubmit} />
+    </div>;
 }
 
 export default Edit;
