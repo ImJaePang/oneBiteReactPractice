29b01a4dc3a127cc61c9765aadc6d000f2300e31
Jaeyoung Im <narushel09@gmail.com>
2026-04-01 12:55:18 +0900

    배포 준비하기 - 중간

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -2,12 +2,18 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import {useNavigate} from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { DiaryDispatchContext } from "../App";
 
 const New = () => {
     const nav = useNavigate();
     const {onCreate} = useContext(DiaryDispatchContext);
+
+    useEffect(()=>{
+        const $title = document.getElementsByTagName("title")[0];
+        $title.innerText = "뉴뉴"
+}, []);
+
     const onSubmit = (input) => {
         onCreate(input.createdDate.getTime(), input.emotionId, input.content);
         nav("/", {replace : true});
